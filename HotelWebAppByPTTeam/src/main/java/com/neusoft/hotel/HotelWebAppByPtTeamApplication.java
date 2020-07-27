242ec7a837d84c72e0192a4506e0fc4b99e31257
lc <540562765@qq.com>
2020-07-27 11:43:34 +0800

    项目第一次提交。

---
+++ HotelWebAppByPTTeam/src/main/java/com/neusoft/hotel/HotelWebAppByPtTeamApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebAppByPtTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWebAppByPtTeamApplication.class, args);
+    }
+
+}
