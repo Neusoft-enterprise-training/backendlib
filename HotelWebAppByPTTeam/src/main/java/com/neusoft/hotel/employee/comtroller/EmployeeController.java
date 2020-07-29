7671e31301fb4c80fc439f01ee3952c21edac35b
shaoyanggit <shaoyang.y@hotmail.com>
2020-07-29 18:17:07 +0800

    杨少阳：更正EmployeeMapper中SQL语句错误；修复EmployeeController

--- HotelWebAppByPTTeam/src/main/java/com/neusoft/hotel/employee/comtroller/EmployeeController.java
+++ HotelWebAppByPTTeam/src/main/java/com/neusoft/hotel/employee/comtroller/EmployeeController.java
@@ -1,27 +1,28 @@
 package com.neusoft.hotel.employee.comtroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neusoft.hotel.Result.Result;
-import com.neusoft.hotel.customer.model.CustomerModel;
-import com.neusoft.hotel.customer.service.ICustomerService;
 import com.neusoft.hotel.employee.model.EmployeeModel;
 import com.neusoft.hotel.employee.service.IEmployeeService;
 
 @RestController
 @RequestMapping("/Employee")
+@CrossOrigin(origins = {"*", "null"})
 public class EmployeeController {
 
 	@Autowired
 	private IEmployeeService employeeService = null;
 
 	@PostMapping(value = "/add")
-	public Result<String> add(EmployeeModel employeeModel) throws Exception {
+	public Result<String> add(@RequestBody EmployeeModel employeeModel) throws Exception {
 		employeeService.add(employeeModel);
 		Result<String> result = new Result<String>();
 		result.setStatus("Fine");
@@ -30,7 +31,7 @@ public class EmployeeController {
 	}
 
 	@PostMapping(value = "/modify")
-	public Result<String> modify(EmployeeModel employeeModel) throws Exception {
+	public Result<String> modify(@RequestBody EmployeeModel employeeModel) throws Exception {
 		employeeService.modify(employeeModel);
 		Result<String> result = new Result<String>();
 		result.setStatus("Fine");
@@ -39,7 +40,7 @@ public class EmployeeController {
 	}
 
 	@PostMapping(value = "/delete")
-	public Result<String> delete(EmployeeModel employeeModel) throws Exception {
+	public Result<String> delete(@RequestBody EmployeeModel employeeModel) throws Exception {
 		employeeService.delete(employeeModel);
 		Result<String> result = new Result<String>();
 		result.setStatus("Fine");
